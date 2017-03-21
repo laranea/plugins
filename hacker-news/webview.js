e831a763b26992ac2ab9b7d4749b6b8c9c8f605e
levels <hello@levels.io>
2017-03-22 02:19:52 +0800

    Open links in new window instead of inside Franz

--- hacker-news/webview.js
+++ hacker-news/webview.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = (Franz, options) => {
 
+  function externalLinks() {
+  for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
+    var b = c[a];
+    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+  }
+}
+;
+externalLinks();
+
   setTimeout(function() {
    location.reload();
   },60000);
