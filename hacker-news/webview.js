acf3a8846593d8835e7302a598a052e5f28dfe9a
levels <hello@levels.io>
2017-03-22 02:23:36 +0800

    Make all links open in new window

--- hacker-news/webview.js
+++ hacker-news/webview.js
@@ -5,7 +5,7 @@ module.exports = (Franz, options) => {
   function externalLinks() {
   for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
     var b = c[a];
-    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+    b.target = "_blank"
   }
 }
 ;
